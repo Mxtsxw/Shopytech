d3b356798c32731418541618ae98f1173ab12200
Matsew-uwu <randriantsoamatthieu@gmail.com>
2022-12-04 19:28:53 +0100

    Création d'une barre de navigation stylisé

--- views/template.php
+++ views/template.php
@@ -4,18 +4,37 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        
         <link rel="stylesheet" href="./static/css/custom.css">
+
         <title><?=$t?></title>
+
+        <!-- Font Awesome -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" />
+        <!-- Google Fonts Roboto -->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" />
+        <!-- MDB -->
+        <link rel="stylesheet" href="./static/css/mdb.min.css" />
     </head>
     <body>
+        <!-- HEADER -->
         <header>
-            <h1><a href="<?= URL ?>">Shopytech</a></h1>
+            <?php require("header.php"); ?>
         </header>
 
-        <?=$content?>
+        <!-- GENERATED CONTENT -->
+        <main>
+            <?=$content?>
+        </main>
         
+        <!-- FOOTER -->
         <footer>
             <p>Le footer</p>
         </footer>      
+
+        <!-- MDB -->
+        <script type="text/javascript" src="js/mdb.min.js"></script>
+        <!-- Custom scripts -->
+        <script type="text/javascript"></script>
     </body>
 </html>
