c1006ce4361d481f7c5fa8707f26a44fe39124e0
Matsew-uwu <randriantsoamatthieu@gmail.com>
2022-12-29 12:05:02 +0100

    Suppression affichage debug

--- controllers/Router.php
+++ controllers/Router.php
@@ -38,7 +38,6 @@ class Router
                 {
                     throw new Exception('Page introuvable');
                 }
-
             }
             else 
             {
@@ -46,8 +45,6 @@ class Router
                 require_once('controllers/ControllerAccueil.php');
                 $this->_ctrl = new ControllerAccueil($url);
             }
-            echo "URL variable : ";
-            var_dump($url);
         }
         // Gestion des erreurs
         catch (Exception $e)
@@ -56,23 +53,6 @@ class Router
             $this->_view = new View('Error');
             $this->_view->generate(array('errorMsg' => $errorMSG));
             $url = explode('/', filter_var($_GET['url'], FILTER_SANITIZE_URL));
-            echo "URL variable : ";
-            var_dump($url);
-        }
-        finally{
-            $current_url = "http://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-            echo "FULL URL : " . $current_url . "<br>";
-            echo "URL SET : " . isset($_GET['url']) . "<br>";
-            echo "URL : " . $_GET['url'] . "<br>";
-            echo "ID : " . $_GET['id'] . "<br>";
-            echo "ROOT : " . $_SERVER['DOCUMENT_ROOT'] . "<br>";
-            echo "<br>";
-            echo "--";
-            echo "<br>";
-            echo "-- path : " . "<br>";
-            echo dirname($_SERVER["PHP_SELF"]);
-            echo "<br>";
-            echo ROOT;
         }
     }
 }
@@ -85,15 +65,3 @@ class Router
 # - /Admin      →   Tableau de bord administrateur
 # - /Cart       →   Panier
 # - /Category   →   Page affichage des produits par catégorie
-
-
-// Problèmes rencontrées :
-
-// Problème de compréhension - comment la réécriture d'URL fonctionne et la gestion des controlleurs par la suite
-
-// Problème : Gérer les urls
-
-// Problème - Lien de fichier avec chemin relatif ne fonctionne pas → sauf que le fichier root n'est pas celui du projet
-// Solution - Rédéfinir la racine dans .htaccess du site et redéfinir le chemin relatif en chemin absolu
-
-// Problème - Récupérer un produit précis à afficher pour Products
--- index.php
+++ index.php
@@ -12,5 +12,5 @@ $router->routeReq();        // Request la route correspondante
 // 1) .htaccess transforme www.shopytech/<...>  en www.shopytech/index.php?url=<...> 
 // 2) index.php appelle le fichier Router.php
 // 3) En fonction du paramètre URL le bon controlleur est appelé (Si aucun paramètre n'est passé : Accueil)
-// 4) Le controlleur se charge d'appeller la classe View avec en paramètre le nom de la vue à appeler 
+// 4) Le controlleur se charge d'appeller la classe View avec en paramètre le nom de la vue correspondante
 // 5) View génère la bonne vue à l'aide des fichiers view*Action*
