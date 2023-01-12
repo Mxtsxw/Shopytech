18814f89a9b58b31f703e5ab2f29c5ece4822c22
Matsew-uwu <randriantsoamatthieu@gmail.com>
2023-01-12 18:17:43 +0100

    📌Version de rendu

--- debug.php
+++ debug.php
@@ -1,34 +1,3 @@
 <?php
-session_start();
-require_once('models/Model.php');
-require_once('models/Customers.php');
-require_once('models/CustomersManager.php');
-require_once('models/DeliveryAddresses.php');
-require_once('models/DeliveryAddressesManager.php');
-require_once('models/Orders.php');
-require_once('models/OrdersManager.php');
-require_once('models/OrderItems.php');
-require_once('models/OrderItemsManager.php');
-require_once('models/Products.php');
-require_once('models/ProductsManager.php');
-require_once('static/lib/fpdf/invoicePDF.php');
-
-$customerManager = new CustomersManager();
-$orderManager = new OrdersManager();
-$orderItemsManager = new OrderItemsManager();
-$productsManager = new ProductsManager();
 
 ?>
-
-
-<!-- 
-Input mot de passe
-
-<div class="form-group">
-<label for="lastname-input">Mot de passe</label>
-<input type="text" name="password" class="form-control" value="" required/>
-</div>
-
-
-
- -->
